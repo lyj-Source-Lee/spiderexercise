4b58a44bb9b7d7ccf059b279f2943fef68754962
lyj-Source-Lee <49940312+lyj-Source-Lee@users.noreply.github.com>
2019-11-04 18:07:03 +0800

    自动申请

---
+++ new_spider.py
@@ -0,0 +1,76 @@
+from selenium import webdriver
+import time
+
+class SCBSpider(object):
+    def __init__(self):
+        self.browser = webdriver.Chrome()
+        self.url = 'http://www.shichangbu.com'
+        self.msg = '期待与您的合作'
+
+    # 登录
+    def page_log(self):
+        self.browser.get(self.url)
+        # 点击登录
+        self.browser.find_element_by_xpath('/html/body/header/div[1]/div/div/div[2]/div/div/div/a[2]').click()
+        # 用户名
+        input = self.browser.find_element_by_id('lgp_mobile')
+        input.send_keys('13836553687')
+        # 密码
+        input = self.browser.find_element_by_id('lgp_password')
+        input.send_keys('100428Yp')
+        # 登录按钮
+        self.browser.find_element_by_xpath('//*[@id="lgp_btn_login"]').click()
+        time.sleep(2)
+        # 异业合作
+        self.browser.find_element_by_xpath('/html/body/header/div[2]/div/div/menu/ul/li[4]/a').click()
+        time.sleep(2)
+
+    # 发送申请
+    def send_msg(self):
+        # li_list = self.browser.find_elements_by_xpath('//*[@id="company_list_data"]')
+        # for li in li_list:
+        for i in range(15):
+            xp = '//*[@id="company_list_data"]/li[{}]/a/div/div[2]/div[1]'.format(i+1)
+            self.browser.find_element_by_xpath(xp).click()
+            time.sleep(2)
+            # 点击申请按钮
+            self.browser.find_element_by_xpath('//*[@id="btnApply"]').click()
+            time.sleep(0.5)
+            # 填入申请信息
+            self.browser.find_element_by_xpath('//*[@id="applyTextarea"]').send_keys(self.msg)
+            time.sleep(0.5)
+            # 点击申请
+            self.browser.find_element_by_xpath('//*[@id="btn_ok"]').click()
+            time.sleep(1)
+            # 返回上一页
+            self.browser.back()
+            time.sleep(2)
+
+    def main(self):
+        self.page_log()
+        x = True
+        while x:
+            self.send_msg()
+            if self.browser.page_source.find('layui-laypage-next layui-disabled') == -1:
+                self.browser.find_element_by_class_name('layui-laypage-next').click()
+            else:
+                x = False
+
+
+if __name__ == '__main__':
+    spider = SCBSpider()
+    spider.main()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
